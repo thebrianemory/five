832c0a9c9ee8c9436a581cad956eb54f93073e93
thebrianemory <the@brianemory.com>
2018-01-18 12:24:03 -0800

    Ready app for Heroku deploy

--- lib/five_web/channels/user_socket.ex
+++ lib/five_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule FiveWeb.UserSocket do
   # channel "room:*", FiveWeb.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
