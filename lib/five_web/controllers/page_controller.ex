13de0e5d99798cc648a9e3b69e836432f417e8b2
thebrianemory <the@brianemory.com>
2018-01-18 10:55:46 -0800

    Update to check if it is five

--- lib/five_web/controllers/page_controller.ex
+++ lib/five_web/controllers/page_controller.ex
@@ -2,6 +2,12 @@ defmodule FiveWeb.PageController do
   use FiveWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    current_time = get_the_time
+    WhatTime.is_it_five(current_time)
+    render(conn, "index.html", location: location, time: time)
+  end
+
+  defp get_the_time do
+    Time.utc.now().hour
   end
 end
